0fb64cb77a3ad01740deb1842a45719adcba7597
nitin-chandra-28 <nc28072006@gmail.com>
2026-07-17 23:25:38 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,20 @@ import githubRoutes from './routes/githubRoutes.js';
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://git-glimpse-alpha.vercel.app"
+];
 
-app.use(cors({ origin: 'https://git-glimpse-alpha.vercel.app/'}));
+app.use(cors({
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 app.use('/api/github', githubRoutes);
